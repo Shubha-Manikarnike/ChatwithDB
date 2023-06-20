97f6a1ad977b4596008f382de5052f348e264c16
Shubha Manikarnike <shubha.vasudev@gmail.com>
2023-06-20 15:50:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ db_host = "database-1.cojmud51692g.us-east-1.rds.amazonaws.com"
 db_name = "postgres"
 db = SQLDatabase.from_uri(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}")
 #db = SQLDatabase.from_uri(f"postgresql+psycopg2://postgres:{env('DBPASS')}@localhost:5432/{env('DATABASE')}",)
-os.environ['OPENAI_API_KEY'] = "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"
 
 QUERY = """
 Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
@@ -27,29 +26,18 @@ Answer: Final answer here
 """
 
 # Setup the database chain
-def get_prompt(prompt):
-    print("Type 'exit' to quit")
-
-    try:
-        question = QUERY.format(question=prompt)
-        print(db_chain.run(question))
-        return db_chain.run(question)
-    except Exception as e:
-        print(e)
-        return e
-
 def generate_response(txt):
     # Instantiate the LLM model
     llm = OpenAI(temperature=0, openai_api_key=os.environ['OPENAI_API_KEY'])
     #db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
-    # Split text
-    #get_prompt()
-    #return get_prompt(txt)
-    return "Hello World"
+    ques=QUERY.format(question=txt)
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, use_query_checker=True)
+    # Split text    
+    return db_chain.run(ques)
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Chat with your DB')
-st.title('🦜🔗 Chat with your DB')
+st.set_page_config(page_title='🔗 Chat with your DB')
+st.title('🔗 Chat with your DB')
 # Text input
 txt_input = st.text_area('Enter your text', '', height=200)
 # Form to accept user's text input for summarization
